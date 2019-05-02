f794642bfe440a7c1805a3a6fd9bf4bcff9c803f
osmar-vaio <osmarfilho6@gmail.com>
2019-05-01 23:29:01 -0300

    Atualização do script e do diagrama de acordo com a refatoração realizada

--- estrutura_banco.sql
+++ estrutura_banco.sql
@@ -1,7 +1,8 @@
 /*
-	RELAÇÃO DE OPERAÇÕES DOS SISTEMAS (login_operacao)
-    ==================================================
-    Classificação das funcionalidades do sistemas que poderão ser ativadas ou bloqueadas
+	RELAÇÃO DE AUTORIZAÇÕES DOS SISTEMAS (login_autorizacao)
+    ==============================================================
+    Classificação das funcionalidades do sistemas que poderão ser 
+    liberadas ou bloqueadas de acordo com os privilégios (autorizações)
     Exemplo:
     Cadastro de vendedores
     Cadastro de clientes
@@ -11,21 +12,22 @@
     Relatório de contas a pagar
     etc...
 */
-create table login_operacao (
-	id_operacao integer unsigned auto_increment,
+create table login_autorizacao (
+    id_autorizacao integer unsigned auto_increment,
     classificacao varchar(100),
     descricao varchar(200),
     ativo boolean default true,
     versao integer default 0,
-    primary key (id_operacao)
+    primary key (id_autorizacao)
 ) engine=innoDB;
 
 
 
 /*
 	PERFIL DE USUÁRIOS (login_perfil)
-    =================================
-    Grupo de usuários do sistema. Podendo ser agrupados por setor, função, módulos, etc...
+    =======================================
+    Grupo de usuários do sistema. 
+    Podendo ser agrupados por setor, função, módulos, etc...
     Exemplo:
     ADMINISTRADOR
     USUARIO
@@ -35,13 +37,13 @@ create table login_operacao (
     etc...
 */
 CREATE TABLE login_perfil (
-    id_perfil INTEGER UNSIGNED AUTO_INCREMENT,
-    descricao VARCHAR(20),
-    ativo BOOLEAN DEFAULT TRUE,
-    versao INTEGER DEFAULT 0,
-    PRIMARY KEY (id_perfil)
-)  ENGINE=INNODB;
-
+  id_perfil int(10) unsigned NOT NULL AUTO_INCREMENT,
+  descricao varchar(20) DEFAULT NULL,
+  notificar_inclusao_usuario tinyint(4) DEFAULT '0',
+  ativo tinyint(1) DEFAULT '1',
+  versao int(11) DEFAULT '0',
+  PRIMARY KEY (id_perfil)
+) ENGINE=InnoDB;
 
 /*
 	OPERAÇÕES POR PERFIL (login_perfil_operacao)
@@ -50,7 +52,7 @@ CREATE TABLE login_perfil (
     Informação necessária na autorização das funcionalidades.
     Exemplo:
     +----------------+-----------------------------------------------------+
-    | P E R F I L    | O P E R A Ç Ã O                                     | 
+    | P E R F I L    | O P E R A Ç Ã O (privilégios)                       | 
     +----------------+-----------------------------------------------------+
     | MASTER         | Todas as operações                                  | 
     +----------------+-----------------------------------------------------+
@@ -61,22 +63,22 @@ CREATE TABLE login_perfil (
     | etc...         | ...                                                 |
     +----------------+-----------------------------------------------------+
 */
-create table login_perfil_operacao (
+create table login_perfil_autorizacao (
     id_perfil integer unsigned not null,
-    id_operacao integer unsigned not null,
-    primary key (id_perfil, id_operacao)
+    id_autorizacao integer unsigned not null,
+    primary key (id_perfil, id_autorizacao)
 ) engine=innoDB;
 
-alter table login_perfil_operacao add constraint fk_perfil_operacao_perfil
+alter table login_perfil_autorizacao add constraint fk_perfil_autorizacao_perfil
 	foreign key (id_perfil) references login_perfil (id_perfil);
-    
-alter table login_perfil_operacao add constraint fk_perfil_operacao_operacao
-	foreign key (id_operacao) references login_operacao (id_operacao);
+
+alter table login_perfil_autorizacao add constraint fk_perfil_autorizacao_autorizacao
+	foreign key (id_autorizacao) references login_autorizacao (id_autorizacao);
 
 
 /*
 	RELAÇÃO DE USUÁRIOS (login_usuario)
-    ===================================
+    ========================================
     Usuários com autorização de uso do sistema.
 */
 
@@ -110,18 +112,14 @@ create table login_usuario (
     +----------------+-----------------------------------------------------+
 */
 create table login_usuario_perfil (
-	id_usuario integer unsigned,
+    id_usuario integer unsigned,
     id_perfil integer unsigned,
     primary key (id_usuario, id_perfil)
 ) engine=innoDB;
 
 alter table login_usuario_perfil add constraint fk_usuario_perfil_usuario
 	foreign key (id_usuario) references login_usuario (id_usuario);
-    
+
 alter table login_usuario_perfil add constraint fk_usuario_perfil_perfil
 	foreign key (id_perfil) references login_perfil (id_perfil);
-    
-    
-    
 
-    
